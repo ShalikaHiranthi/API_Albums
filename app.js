6d5210139af0dbc0a8edf52c98db758e1d0d4a80
Shalika Hiranthi Dissanayaka <ag1640@student.jamk.fi>
2026-04-05 16:36:46 +0300

    api

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import session from "express-session";
-import MongoStore from "connect-mongo";
 import passport from "passport";
 import dotenv from "dotenv";
 import path from "path";
@@ -8,27 +7,13 @@ import path from "path";
 import albumRoutes from "./routes/albums.js";
 import authRoutes from "./routes/auth.js";
 import "./config/passport.js"; // load strategy
-import connectMongoDB from "./db/mongodb.js";
 
 dotenv.config();
 const app = express();
 
-const MONGO_URI = process.env.MONGO_URI;
-const PORT = process.env.PORT;
-
 app.use(express.json());
 app.use(express.static(path.join(path.resolve(), "public")));
 
-// Session middleware
-// app.use(
-//   session({
-//     secret: "mysecret",
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-//     cookie: { maxAge: 1000 * 60 * 60 },
-//   }),
-// );
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -46,8 +31,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use("/albums", albumRoutes);
 app.use("/api", authRoutes);
+app.use("/api/albums", albumRoutes);
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -55,18 +40,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ msg: err.message || "Server error" });
 });
 
-// const startServer = async () => {
-//   try {
-//     await connectMongoDB(MONGO_URI);
-//     app.listen(PORT, () => {
-//       console.log(`Server running on http://localhost:${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server:", error);
-//     // graceful shutdown
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
 export default app;
